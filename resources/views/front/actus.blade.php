54fba56fc3cb0868e150227c22de86ea4d0b53f0
noumecha spaker <noumechaivan@gmail.com>
2024-09-21 05:45:18 +0100

    maj

---
+++ resources/views/front/actus.blade.php
@@ -0,0 +1,118 @@
+<x-front-layout>
+    <section class="bg-02-a">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="_head_01">
+                        <h2>Actualités</h2>
+                        <p>Acceuil<i class="fas fa-angle-right"></i><span>Articles & Publication</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-04">
+        <div class="container">
+            <div class="row">
+               <div class="col-12">
+                    <div class="heading">
+                        <h2>Latest News</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <article class="_lk_bg_sd_we">
+                      <div class="_bv_xs_we"></div>
+                      <div class="_xs_we_er">
+                        <div class="_he_w">
+                          <h3>Easy English Learning Way</h3>
+                          <ol>
+                            <li><span>by</span> admin<span class="_mn_cd_xs">june 30, 2020</span></li>
+                          </ol>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                        </div>
+                      </div>
+                    </article>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <article class="_lk_bg_sd_we">
+                      <div class="_bv_xs_we" style="{{ asset('front/images/blog/img-01.jpg') }}"></div>
+                      <div class="_xs_we_er">
+                        <div class="_he_w">
+                          <h3>Summer Course Start From 1st June</h3>
+                          <ol>
+                            <li><span>by</span> admin<span class="_mn_cd_xs">june 30, 2020</span></li>
+                          </ol>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                        </div>
+                      </div>
+                    </article>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <article class="_lk_bg_sd_we">
+                        <div class="_bv_xs_we" style="{{ asset('front/images/blog/img-03.jpg') }}"></div>
+                      <div class="_xs_we_er">
+                        <div class="_he_w">
+                          <h3>Guest Interview will Occur Soon</h3>
+                          <ol>
+                            <li><span>by</span> admin<span class="_mn_cd_xs">june 30, 2020</span></li>
+                          </ol>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                        </div>
+                      </div>
+                    </article>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <article class="_lk_bg_sd_we">
+                        <div class="_bv_xs_we" style="{{ asset('front/images/blog/img-04.jpg') }}"></div>
+                      <div class="_xs_we_er">
+                        <div class="_he_w">
+                          <h3>Guest Interview will Occur Soon</h3>
+                          <ol>
+                            <li><span>by</span> admin<span class="_mn_cd_xs">june 30, 2020</span></li>
+                          </ol>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                        </div>
+                      </div>
+                    </article>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <article class="_lk_bg_sd_we">
+                        <div class="_bv_xs_we" style="{{ asset('front/images/blog/img-05.jpg') }}"></div>
+                      <div class="_xs_we_er">
+                        <div class="_he_w">
+                          <h3>Guest Interview will Occur Soon</h3>
+                          <ol>
+                            <li><span>by</span> admin<span class="_mn_cd_xs">june 30, 2020</span></li>
+                          </ol>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                        </div>
+                      </div>
+                    </article>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <article class="_lk_bg_sd_we">
+                        <div class="_bv_xs_we" style="{{ asset('front/images/blog/img-06.jpg') }}"></div>
+                      <div class="_xs_we_er">
+                        <div class="_he_w">
+                          <h3>Guest Interview will Occur Soon</h3>
+                          <ol>
+                            <li><span>by</span> admin<span class="_mn_cd_xs">june 30, 2020</span></li>
+                          </ol>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                        </div>
+                      </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-front-layout>
