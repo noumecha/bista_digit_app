021d32748ef3e0fbe61a7e06bb5c67dbc4274d14
noumecha spaker <noumechaivan@gmail.com>
2024-12-15 14:58:44 +0100

    maj

--- public/js/functions/migrate.js
+++ public/js/functions/migrate.js
@@ -7,41 +7,40 @@ $(function(){
         var form_method = $(this).closest('form').prop('method');
         var form_action = $(this).closest('form').prop('action');
         var modal_id = $(this).closest('div.modal').prop('id');
-        //setTimeout(() => {
-            $.ajax({
-                url: form_action,
-                type: form_method,
-                data: form_datas,
-                success: function(response) {
-                    if(response.error)
-                        setSuccessMessage(response.error, '#modal-alert-success');
-                    if(response.success)
-                        setSuccessMessage(response.success, '#modal-alert-success');
-                    console.log(response);
+        var show_error_msg_block = $(this).closest('#modal-alert-success')
+        $.ajax({
+            url: form_action,
+            type: form_method,
+            data: form_datas,
+            success: function(response) {
+                if(response.error)
+                    setSuccessMessage(response.error, show_error_msg_block);
+                if(response.success)
+                    setSuccessMessage(response.success, '#modal-alert-success');
+                spinner.addClass('d-none');
+            },
+            error: function(xhr, status, error) {
+                var datas = Object.entries(xhr.responseJSON.errors);
+                var errors = datas.map(error => error[1][0]);
+                $('#'+modal_id).on('hidden.bs.modal', function() {
+                    return false;
+                });
+                setTimeout(function() {
                     spinner.addClass('d-none');
-                },
-                error: function(xhr, status, error) {
-                    //console.error(xhr);
-                    var datas = Object.entries(xhr.responseJSON.errors);
-                    var errors = datas.map(error => error[1][0]);
-                    $('#'+modal_id).on('hidden.bs.modal', function() {
-                        return false;
-                    });
-                    setTimeout(function() {
-                        spinner.addClass('d-none');
-                    }, 1000);
-                    setSuccessMessage(errors, '#modal-alert-errors');
-                }
-            });
-        //}, 2000);
+                }, 1000);
+                setSuccessMessage(errors, '#modal-alert-errors');
+            }
+        });
     });
 
     // success function
     function setSuccessMessage(msg, id) {
         var msgBlock = $(id);
-        msgBlock.stop(true, true);
+        msgBlock.removeClass('d-none')
+        console.log(msgBlock.attr('class'));
+        /*msgBlock.stop(true, true);
         msgBlock.empty();
-        if (Array.isArray(msg)) {
+       if (Array.isArray(msg)) {
             var list = $('<ul class="list-group text-left"></ul>');
             msg.forEach(function(m) {
                 var items = $('<li class="list-group-item list-group-item-danger"></li>').text(m);
@@ -55,6 +54,6 @@ $(function(){
         msgBlock.fadeIn().css('display', 'block');
         setTimeout(function() {
             msgBlock.fadeOut();
-        }, 3000);
+        }, 3000);*/
     }
 });
